d0b7478a58b7daedd40520e585cc34f874e67c44
pavelrzn <0p9o8i7u6y-->
2020-09-06 21:29:25 +0300

    omfg

--- src/main/java/com/example/AsusBoss/AsusBossApplication.java
+++ src/main/java/com/example/AsusBoss/AsusBossApplication.java
@@ -3,6 +3,7 @@ package com.example.AsusBoss;
 
 import com.example.AsusBoss.service.TlgBot;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
@@ -10,21 +11,25 @@ import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
-public class AsusBossApplication {
+public class AsusBossApplication implements CommandLineRunner {
 
+	TlgBot bot;
 	@Autowired
-	static TlgBot bot;
+	public AsusBossApplication(TlgBot bot) {
+		this.bot = bot;
+	}
 
 	static {
 		ApiContextInitializer.init();
 	}
 
-	public static void main(String[] args) throws TelegramApiRequestException {
-		ApiContextInitializer.init();
-		TelegramBotsApi botsApi = new TelegramBotsApi();
-		botsApi.registerBot(bot);
-
+	public static void main(String[] args) {
 		SpringApplication.run(AsusBossApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		botsApi.registerBot(bot);
+	}
 }
